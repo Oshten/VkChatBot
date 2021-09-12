dc3ee95edcab5a4aa35add663e3c427f4781dd56
Oshten <mereguan55@yandex.ru>
2021-09-12 21:16:29 +0300

    Пропосываю получение сообщений

--- vk_chatbot.py
+++ vk_chatbot.py
@@ -1,14 +1,25 @@
 #! engoding = utf8
 
-from vk_api import bot_longpoll
+from vk_api import vk_api, bot_longpoll
+import _key
+
+group_id = 207094096
+token = _key._access_key
 
 class Bot:
 
-    def __init__(self):
-        pass
+    def __init__(self, token, group_id):
+        self.token = token
+        self.group_id = group_id
+        self.vk = vk_api.VkApi(token=self.token)
+        self.longpoller = bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=group_id, wait=60)
 
     def run(self):
-        pass
+        for event in self.longpoller.listen():
+            print(event.message)
 
 
+if __name__ == '__main__':
+    bot = Bot(token=token, group_id=group_id)
+    bot.run()
 
