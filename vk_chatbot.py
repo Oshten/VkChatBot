7db796eb7529aa04ca13e19bfa5b5b06e95b2ece
Oshten <mereguan55@yandex.ru>
2021-09-19 16:46:22 +0300

    Запускаю настроенный ответ

--- vk_chatbot.py
+++ vk_chatbot.py
@@ -14,31 +14,34 @@ class Bot:
         self.group_id = group_id
         self.vk = vk_api.VkApi(token=self.token)
         self.longpoller = bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=group_id, wait=60)
+        self.base_names = []
+        # TODO Добавить время отключения
 
     def run(self):
         for event in self.longpoller.listen():
+            if event.type == bot_longpoll.VkBotEventType.MESSAGE_TYPING_STATE:
+                user_name = self._find_username(event)
+                if user_name:
+                    self.event_processing(
+                        message_answer=f'Тебя приветствую, падаван, {user_name}!',
+                        event=event
+                    )
             if event.type == bot_longpoll.VkBotEventType.MESSAGE_NEW:
-                self.event_processing(event=event)
+                message = event.message.text
+                self.event_processing(
+                    message_answer='sss',
+                    event=event
+                )
             else:
                 print('Не умею обрабатывать такие события', event.type)
 
 
-    def event_processing(self, event):
-        message = event.message.text
+    def event_processing(self, message_answer, event):
         try:
-            user_name, user_lastname, answer = self._find_username(event)
-            if answer:
-                if user_name:
-                    message_answeer = f'Тебя приветствую, падаван, {user_name}!'
-                else:
-                    message_answeer = 'Тебя приветствую, падаван!\nТемны мысли твои. Кто ты?'
-            else:
-                message_answeer = f'Тебя приветствую, падаван, {user_name} {user_lastname}!'
-
             self.vk.method(
                 method='messages.send',
                 values={
-                    'message' : message_answeer,
+                    'message' : message_answer,
                     'random_id' : randint(1, 2**50),
                     'peer_id' : event.message.peer_id
                 }
@@ -48,13 +51,47 @@ class Bot:
 
     def _find_username(self, event):
         users_info = self.vk.method(method='users.get', values={'user_ids': event.message.from_id})
-        users_name, user_lastname, answer = None, None, None
+        users_name = None
         try:
             user_name = users_info[0]['first_name']
-            user_lastname = users_info[0]['last_name']
+            user_name += ' ' + users_info[0]['last_name']
         except KeyError:
-            answer = 'Кто ты, падаван?'
-        return user_lastname, user_name, answer
+            if user_name:
+                print ('Фамилия не указана')
+            else:
+                print('Имя и фамилия не указаны')
+        return user_name
+
+    def _check_username(self, user_name):
+        if user_name in self.base_names:
+            return True
+        elif user_name:
+            self.base_names.append(user_name)
+            return False
+        return False
+
+    def _find_message(self, user_name, message):
+        result_check_username = self._check_username(user_name=user_name)
+        result_check_message = self._check_message(message=message)
+        if result_check_username:
+            if not result_check_message:
+                get_message = message.strip().replace(',', '').replace('.', '').replace('бот', '')
+                message_list = get_message.lower().split(' ')[::-1]
+                message_list[0] = message_list[0].title()
+                messege_answer = ' '.join(message_list) + ', падаван.'
+            elif result_check_message == 'question_who':
+                messege_answer = 'Йодабот я, падаван.'
+        elif not user_name:
+            messege_answer = 'Темны мысли твои. Кто ты?'
+        else:
+            message_answer = f'Тебя приветствую, падаван, {user_name}!'
+        return messege_answer
+
+    def _check_message(self, message):
+        if 'кто' and 'ты' in message.lower():
+            return 'question_who'
+        return False
+
 
 
 if __name__ == '__main__':
