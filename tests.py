183619b4940eaaa1898958e9e20051db6608ce54
Oshten <mereguan55@yandex.ru>
2021-11-02 00:35:58 +0300

    Пишем второй тест

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ from vk_chatbot import Bot
 
 class Test_bot(TestCase):
 
-    def test_run_start(self):  # Проверка что функция run вызывает следующую функцию
+    def test_run_event_mesage_typing_state(self):  # Проверка что функция run вызывает следующую функцию
         count = 5
         event = Mock()
         event.type = 'message_typing_state'
@@ -25,7 +25,28 @@ class Test_bot(TestCase):
                     bot.run()
 
                     bot.event_processing.assert_called()
-                    # bot.event_processing.assert_called_with(event)
+                    bot._find_username.assert_called_with(event)
+                    self.assertEqual(bot.event_processing.call_count, count)
+
+
+    def test_run_start(self):  # Проверка что функция run вызывает следующую функцию
+        count = 5
+        event = Mock()
+        event.type = 'message_new'
+        events = [event]*count       # [{}, {}, ...]
+        longpoller_mock = Mock(return_value=events)
+        longpoller_listen_mock = Mock()
+        longpoller_listen_mock.listen = longpoller_mock
+        with patch('vk_chatbot.vk_api.VkApi'):
+            with patch('vk_chatbot.bot_longpoll.VkBotLongPoll', return_value=longpoller_listen_mock):
+                with patch('vk_chatbot.bot_longpoll.VkBotEventType') as vk_bot_event_type:
+                    bot = Bot('', '')
+                    vk_bot_event_type.MESSAGE_NEW = 'message_new'
+                    bot._find_message = Mock(user_name='Вася Пупкин', return_value='Привет')
+                    bot.event_processing = Mock()
+                    bot.run()
+
+                    bot.event_processing.assert_called()
                     self.assertEqual(bot.event_processing.call_count, count)
 
 
